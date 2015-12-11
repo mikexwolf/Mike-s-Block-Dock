e80a06e8f9cfe932032f3aa86bc77b5469fe44b4
mikexwolf <mshainski@georgebrown.ca>
2015-12-11 10:37:36 -0600

    With array

--- js/main.js
+++ js/main.js
@@ -1,20 +1,56 @@
+// var main = function() {  
+
 $(document).ready(function(e){
 
-		$(".block").draggable({
-			snap: '.box',
-    		snapMode: 'inner'
-		});
-		
-		$( ".box" ).droppable(
 
-		{
-			accept:".block",
-			drop: function (ev, ui) {
+	var letters = [
+		["A", "N", "A", "G", "R", "A", "M"],
+		["W", "O", "R", "D"],
+		["D", "R", "A", "M", "A"],
+		["D", "O", "O", "R"]
+	];
+	
+	var word = letters[Math.floor(Math.random() * letters.length)];
 
-				$(this).append(droppedItem);
 
-			}
-		}
+	$(".block").each( function ( i ) {
+    $( this ).text( word[i] );
+	});
 
-			);
+
+	for (var i = 0; i < word.length; i++){
+		$('#boxes').append($('<div class="box" data-letter="'+word[i]+'"></div>'));
+	}
+
+	word.sort(function() { return 0.5 - Math.random() });
+
+	for (var i = 0; i < word.length; i++) {
+		var value = $('#blocks').append($('<div class="block" data-letter="'+word[i]+'"></div>').text(word[i]));
+	}
+
+	
+
+    $(".block").draggable( {
+		containment: 'document',
+		snap: '.box',
+		snapMode: 'inner'
+	});
+
+	$(".box").droppable( {
+	    drop: dropEvent,
+	    accept: ".block"
+  	});
+
+	function dropEvent( event, ui ) {
+  	var draggableId = ui.draggable.attr("data-letter");
+  	var droppableId = $(this).attr("data-letter");
+  	var draggable = ui.draggable;
+  	if (draggableId === droppableId) {
+  		$(this).append(droppedItem);
+  	};
+	}
+	
 });
+
+// };
+// $(document).ready(main);
